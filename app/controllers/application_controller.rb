5fcbefa1f48e7b127b592e8b4b6eecf3507c3a38
Shinryuzz <mshin1127@gmail.com>
2023-08-01 11:39:27 +0900

    add firebase auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::API
+  include FirebaseAuth
+  include Api::ExceptionHandler
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  before_action :authenticate_token
+
+  def authenticate_token
+    authenticate_with_http_token do |token, _options|
+      result = verify_id_token(token)
+
+      if result[:errors]
+        render_400(nil, result[:errors])
+      else
+        @_current_user = User.find_or_create_by!(uid: result[:uid])
+      end
+    end
+  end
+
+  def current_user
+    @_current_user
+  end
 end
